1b138e653afd7d16cf4445b7fea6b3c6d7907329
Sherin <68326500+Sherinsedalo@users.noreply.github.com>
2025-03-17 10:26:20 +0100

    adding font import

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
     <>
       <Navbar />
       <section className="max-w-screen mx-auto  py-6 bg-[#F8F2E9]">
-        <main className="pt-24 px-6 flex gap-8 md:gap-16 leading-8  sm:px-10 md:px-16 font-sans m-30 ">
+        <main className="pt-24 px-6 flex gap-8 md:gap-16 leading-8  sm:px-10  md:px-16 font-sans m-30 ">
           <img
             src="src/assets/selfie.jpg"
             className="w-40 h-40 rounded-4xl shadow-lg sm:w-48 sm:h-48  md:h-60 md:w-60 mx-auto sm:mx-0"
           ></img>
-          <div className="text-[#6D4C41] flex flex-col gap-4">
+          <div className="text-[#6D4C41] flex flex-col gap-4 ">
             <p className="text-lg md:text-xl">
               Being originally from the Ivory Coast and born in France, when I
               arrived I was always on the lookout for West African restaurants.
